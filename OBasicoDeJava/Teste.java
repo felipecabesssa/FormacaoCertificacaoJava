3e383115e21282a4472e6ee095a06b29a938ec64
felipecabesssa <felipesoares100@hotmail.com>
2021-08-23 18:34:18 -0300

    Criando pasta OBasicoDeJava e primeiro commit

---
+++ OBasicoDeJava/Teste.java
@@ -0,0 +1,17 @@
+public class Teste {
+    public void m1(){
+        int x = 10;
+
+        if(x >= 10) {
+            int y = 50;
+            System.out.println(y);
+        }
+
+        System.out.println(x);
+    }
+
+    public static void main(String[] args) {
+        Teste t = new Teste();
+        t.m1();
+    }
+}
