b6daa8d964d98fd0b0ab4aaaaf6a4021f758973c
felipecabesssa <felipesoares100@hotmail.com>
2021-08-25 14:43:36 -0300

    variaveis de referencia e primitivas

---
+++ TiposDeDados/2DiferencaEntreVariaveisDeReferenciaAObjetosETiposPrimitivos/TesteReferencia.java
@@ -0,0 +1,23 @@
+class ObjetoMeu {
+    int valor;
+}
+
+public class TesteReferencia {
+    public static void main(String[] args){
+        ObjetoMeu o1 = new ObjetoMeu();
+        o1.valor = 10;
+        ObjetoMeu o2 = o1;
+
+        o1.valor = o1.valor + 9;
+
+        System.out.println(o1.valor);
+        System.out.println(o2.valor);
+
+        int a = 10;
+        int b = a;
+
+        a= a + 5;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
