b2e4faf00d060b16e6549dde4f3f1039cae37f45
felipecabesssa <felipesoares100@hotmail.com>
2021-08-26 19:04:21 -0300

    Operadores de comparação

---
+++ Operadores/03-Operadores de comparacao/TestaComparacao.java
@@ -0,0 +1,15 @@
+public class TestaComparacao {
+    public static void main(String[] args) {
+        /*
+         * Uma comparação pode devolver dois valores possíveis: verdadeiro ou falso. No
+         * Java, uma comparação sempre devolve um valor boolean.
+         */
+
+        System.out.println(1 == 1); // true.
+        System.out.println(1 != 1); // false.
+        System.out.println(2 < 1); // false.
+        System.out.println(2 > 1); // true.
+        System.out.println(1 >= 1); // true.
+        System.out.println(2 <= 1); // false.
+    }
+}
